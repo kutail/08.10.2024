2260c9b7392e906fcd7c48e36ec559b424b3dc4c
kutail <kutep0v.vlad.m@gmail.com>
2024-10-08 13:44:33 +0500

    kalk

--- 08.10.2024/main.cpp
+++ 08.10.2024/main.cpp
@@ -1,14 +1,87 @@
-//
-//  main.cpp
-//  08.10.2024
-//
-//  Created by Владислав on 08.10.2024.
-//
-
 #include <iostream>
+#include <cstdlib>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+using namespace std;
+
+double Deleniye(double x1, double x2)
+{
+    
+    return x1/x2;
+}
+
+double Umn(double x1, double x2)
+{
+    
+    return x1*x2;
+}
+
+double Plus(double x1, double x2)
+{
+    
+    return x1+x2;
+}
+
+double Minus(double x1, double x2)
+{
+    
+    return x1-x2;
+}
+
+double Proc(double x1, double x2)
+{
+    
+    return x1/x2*100;
+}
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+    
+    double x1=0;
+    double x2=0;
+    double itog=0;
+    char znak;
+    
+    cout << "введи число: " << endl;
+    cin >> x1;
+    
+    cout << "введи операцию (+, -, *, /, %): ";
+    cin >> znak;
+    
+    cout << "введи второе число: " << endl;
+    cin >> x2;
+    
+    if (znak== '+')
+    {
+        Plus(x1, x2);
+    }
+    else if (znak == '-')
+    {
+        Minus(x1, x2);
+    }
+    else if (znak == '*')
+    {
+        Umn(x1, x2);
+    }
+    else if (znak == '/')
+    {
+        if (x2 != 0)
+        {
+            Deleniye(x1, x2);
+        }
+        else
+        {
+            cout << "ошибка: Деление на ноль!" << endl;
+        }
+    }
+    else if (znak == '%')
+    {
+        Proc(x1, x2);
+    }
+    else
+    {
+        cout << "ввыбрана не верная операция" << endl;
+    }
+    
     return 0;
 }
